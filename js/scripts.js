a8ecf8dfd8577f1135d0e9c3b2a8bcaf7a43a43d
Alexander Kondra and Lake Bramlett <lake@>
2019-06-03 14:54:55 -0700

    finshed cipher js value

--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,17 @@ var prompt = prompt("Enter a sentence");
 var letters = prompt.charAt(0).toUpperCase() + prompt.charAt(prompt.length-1).toUpperCase();
 var letterArray = Array.from(letters);
 var reverse = letterArray.reverse().join('');
+var middleChar = prompt.charAt(Math.floor(prompt.length/2));
+var finalCipher = middleChar + prompt + reverse;
 
 
 
 console.log('this prompt is: ', prompt);
 console.log('our first and last letters are: ', letters);
 console.log('those letters reversed are: ', reverse);
+console.log('the middle character is: ', middleChar);
 
-alert(prompt + reverse);
+alert(finalCipher);
 
 
 
